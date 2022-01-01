5f26b28ba100ee41ed959ca25ba7ba7195cd4c11
Noah VanSickle <vansickn@union.edu>
2022-01-01 17:48:28 -0600

    added videbackground okg

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import '../index.css';
 import router from './router';
 import Card from './components/Card.vue';
+import VideoBackground from 'vue-responsive-video-background-player';
 
 const app = createApp(App)
 app.use(router)
+app.component('video-background',VideoBackground)
 app.component('Card', Card)
 
 const mountedApp = app.mount('#app')
