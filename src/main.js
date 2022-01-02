c7ec002fad00a4ab3501c19bb1838d98d04c4c0b
Noah VanSickle <vansickn@union.edu>
2022-01-01 20:00:55 -0600

    added modal component

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import router from './router';
 import Card from './components/Card.vue';
 import VideoBackground from 'vue-responsive-video-background-player';
 
+// Universal Modal Component
+import 'vue-universal-modal/dist/index.css'
+import VueUniversalModal from 'vue-universal-modal'
+
 const app = createApp(App)
 app.use(router)
+
+// use Vue Universal Modal Package
+app.use(VueUniversalModal, {
+  teleportTarget: 'body'
+})
+
 app.component('video-background',VideoBackground)
 app.component('Card', Card)
 
